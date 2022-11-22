5498364bfad4dac8f737b041102903009236b981
KGrants <118312276+KGrants@users.noreply.github.com>
2022-11-22 22:32:11 +0200

    Update Power of Thor - Episode 1.cs

--- Power of Thor - Episode 1/Power of Thor - Episode 1.cs
+++ Power of Thor - Episode 1/Power of Thor - Episode 1.cs
@@ -1,50 +1,31 @@
 using System;
-using System.Linq;
-using System.IO;
-using System.Text;
-using System.Collections;
-using System.Collections.Generic;
 
 class Player
 {
     static void Main(string[] args)
     {
         string[] inputs = Console.ReadLine().Split(' ');
-        int lightX = int.Parse(inputs[0]); // the X position of the light of power
-        int lightY = int.Parse(inputs[1]); // the Y position of the light of power
-        int thorX = int.Parse(inputs[2]); // Thor's starting X position
-        int thorY = int.Parse(inputs[3]); // Thor's starting Y position
+        int Xcor = int.Parse(inputs[0])-int.Parse(inputs[2]);
+        int Ycor = int.Parse(inputs[1])-int.Parse(inputs[3]);
 
         while (true)
         {
             int remainingTurns = int.Parse(Console.ReadLine()); // The remaining amount of turns Thor can move. Do not remove this line.
-
-            string directionX = "";
-            string directionY = "";
-            
-            if (thorX > lightX)
-            {
-                directionX = "W";
-                thorX--;
-            }
-            else if (thorX < lightX)
-            {
-                directionX = "E";
-                thorX++;
-            }
-            
-            if (thorY > lightY)
-            {
-                directionY = "N";
-                thorY--;
-            }
-            else if (thorY < lightY)
-            {
-                directionY = "S";
-                thorY++;
-            }
-
-            Console.WriteLine(directionY + directionX);
+            string move = getdirection(Xcor, Ycor);
+            if (move.Contains('W')) Xcor += 1;
+            if (move.Contains('E')) Xcor -= 1;
+            if (move.Contains('N')) Ycor += 1;
+            if (move.Contains('S')) Ycor -= 1;
+            Console.WriteLine(move);
         }
+    }
+    static string getdirection(int x, int y)
+    {
+        string result = "";
+        if (y<0) result += 'N';
+        if (y>0) result += 'S';
+        if (x<0) result += 'W';
+        if (x>0) result += 'E';
+        return result;
     }
 }
