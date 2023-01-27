594ad026b8a1c68b54fc009ae8f5deb51ba2b33a
KGrants <118312276+KGrants@users.noreply.github.com>
2023-01-28 01:39:05 +0200

    Create Game of Battleships.cs

---
+++ Clash of Code/Game of Battleships/Game of Battleships.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(n*n/3);
+    }
+}
