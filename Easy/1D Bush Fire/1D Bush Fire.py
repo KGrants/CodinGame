639481e46a24e6172c1ab9daaa7909a6f37e5bce
KGrants <118312276+KGrants@users.noreply.github.com>
2023-02-01 00:12:56 +0200

    Create 1D Bush Fire.py

---
+++ Easy/1D Bush Fire/1D Bush Fire.py
@@ -0,0 +1,11 @@
+n = int(input())
+for i in range(n):
+    answer = 0
+    u = 0
+    line = input()
+    while u < len(line):
+        if line[u] == 'f':
+            answer+=1
+            u+=2
+        u+=1
+    print(answer)
