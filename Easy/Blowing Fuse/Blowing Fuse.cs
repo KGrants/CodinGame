e84e3a3de0d3caafb61e1dff8be8d9f82466c88d
KGrants <118312276+KGrants@users.noreply.github.com>
2023-02-06 00:01:51 +0200

    Update Blowing Fuse.cs

--- Easy/Blowing Fuse/Blowing Fuse.cs
+++ Easy/Blowing Fuse/Blowing Fuse.cs
@@ -1,38 +1,51 @@
 using System;
-using System.Linq;
-using System.Collections.Generic;
 
 class Solution
 {
     static void Main(string[] args)
     {
-        string[] inputs;
-        inputs = Console.ReadLine().Split(' ');
-        int n = int.Parse(inputs[0]);
-        int m = int.Parse(inputs[1]);
-        int c = int.Parse(inputs[2]);
-        int result = 0;
+        Func<int[]> getinput = () => Array.ConvertAll(Console.ReadLine().Trim().Split(' '),Convert.ToInt32);;
+        int [] inputs   = getinput();
+        int n           = inputs[0];
+        int m           = inputs[1];
+        int c           = inputs[2];
+        int power       = 0;
+        int max_power   = 0;
 
-        Dictionary<int, int> power = new Dictionary<int, int>();
-        inputs = Console.ReadLine().Split(' ');
-        for (int i = 1; i <= n; i++)
-            power.Add(i,int.Parse(inputs[i-1]));
+        //Read and create an array of Appliances
+        inputs = getinput();
+        Appliance[] app_list = new Appliance[n];
+        for (int i = 0; i < n; i++)
+            app_list[i] = new Appliance(inputs[i], false);
 
-        List<int> maxpower = new List<int>();
-        inputs = Console.ReadLine().Split(' ');
+        //Read and press power buttons
+        inputs = getinput();
         for (int i = 0; i < m; i++)
         {
-            result += power[int.Parse(inputs[i])];
-            maxpower.Add(result);
-            power[int.Parse(inputs[i])] *= -1;
-        }
+            Appliance x = app_list[inputs[i]-1];
+            power += x.turned_on_ind == false ?  x.power : -x.power;
+            app_list[inputs[i]-1].turned_on_ind = x.turned_on_ind == false ? true : false;
+            max_power = power > max_power ? power : max_power;
 
-        if(maxpower.Max()>c)
-            Console.WriteLine("Fuse was blown.");
-        else
+            if(max_power>c)
+            {
+                Console.WriteLine("Fuse was blown.");
+                System.Environment.Exit(0);  
+            }
+        }
+        Console.WriteLine($"Fuse was not blown.");
+        Console.WriteLine($"Maximal consumed current was {max_power} A.");
+    }
+
+    class Appliance
+    {   
+        public int power {get; set;}
+        public bool turned_on_ind {get; set;}
+
+        public Appliance(int pow, bool turned_on)
         {
-            Console.WriteLine($"Fuse was not blown.");
-            Console.WriteLine($"Maximal consumed current was {maxpower.Max()} A.");
+            power = pow;
+            turned_on_ind = turned_on;
         }
     }
 }
