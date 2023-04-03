2230fa271a99ade51f8c7bc0d0e09f31e2451e39
Karlis Grants <118312276+KGrants@users.noreply.github.com>
2023-04-03 14:55:27 +0300

    Add files via upload

---
+++ Clash of Code/Count People/Count People.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x; cin.ignore();
+    cout << x+x*x+x*x*x << endl;
+}
