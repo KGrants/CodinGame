7027a4443cca3097c721ea56cf846afb6ad8fadc
KGrants <118312276+KGrants@users.noreply.github.com>
2023-06-27 18:02:52 +0300

    Create Find Divisors.py

---
+++ Clash of Code/Find divisors/Find Divisors.py
@@ -0,0 +1,16 @@
+import math
+
+def divisorGenerator(n):
+    large_divisors = []
+    for i in range(1, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            yield i
+            if i*i != n:
+                large_divisors.append(n / i)
+    for divisor in reversed(large_divisors):
+        yield divisor
+
+a = list(divisorGenerator(int(input())))
+a.sort(reverse=True)
+b = [int(i) for i in a]
+print(*b)
